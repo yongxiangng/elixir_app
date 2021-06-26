3f51b4b382fc08fc4e4a6163e1fab56e82d473a1
yongxiangng <yongxiangng@gmail.com>
2021-06-27 00:47:02 +0800

    Add button to update database

--- apps/elixir_app_web/assets/js/react/App.js
+++ apps/elixir_app_web/assets/js/react/App.js
@@ -1,27 +1,36 @@
 import ModuleBubble from "./components/ModuleBubble";
 import React, { useState, useEffect } from "react";
-import { Input } from "antd";
+import { Input, Row, Col, Button } from "antd";
 
 import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
 import "./index.css";
 
 function App() {
-  const [mods, setMods] = useState([])
+  const [mods, setMods] = useState([]);
+  const local = `http://${window.location.hostname}:${window.location.port}/api/v1/list_all_modules`;
+  const remote = `http://${window.location.hostname}:${window.location.port}/api/v1/update_modules`;
 
   useEffect(() => {
-    getMods();
-  }, [])
+    getMods(local);
+  }, []);
 
-  const getMods = async () => {
-    const res = await fetch(`http://${window.location.hostname}:${window.location.port}/api/v1/list_all_modules`)
-    const data = await res.json();
-    setMods(data);
-  }
+  const getMods = (url) => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setMods(data));
+  };
 
   return (
     <div>
       Hello this is react!
-      <Input placeholder="Add module" />
+      <Row gutter={16} align="middle" justify="center">
+        <Col span={18}>
+          <Input placeholder="Add module" />
+        </Col>
+        <Col span={6}>
+          <Button onClick={() => getMods(remote)}>Update Modules</Button>
+        </Col>
+      </Row>
       {mods.map((mod, key) => (
         <ModuleBubble key={key} {...mod} />
       ))}
