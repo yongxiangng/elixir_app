yongxiangng/elixir_app

commit 17bbe1c3e6515686879ad2253c806ccaa3e9e263

Add selection bar for modules

apps/elixir_app_web/assets/js/react/components/SelectModules.js
@@ -1,0 +1,32 @@
+import React from "react";
+import { Select } from "antd";
+
+function SelectModules(props) {
+  const { Option } = Select;
+  const { mods } = props;
+
+  const children = [];
+  mods.forEach((mod) => {
+    children.push(
+      <Option key={`${mod.module_code} ${mod.module_title}`}>
+        {`${mod.module_code} ${mod.module_title}`}
+      </Option>
+    );
+  });
+
+  return (
+    <>
+      <Select
+        mode="multiple"
+        allowClear
+        style={{ width: "100%" }}
+        placeholder="Select your modules here"
+        onChange={handleChange}
+      >
+        {children}
+      </Select>
+    </>
+  );
+}
+
+export default SelectModules;

apps/elixir_app_web/assets/js/react/App.js
@@ -1,7 +1,8 @@
 import ModuleTable from "./components/ModuleTable";
 import UpdateModuleButton from "./components/UpdateModuleButton";
+import SelectModules from "./components/SelectModules";
 import React, { useState, useEffect } from "react";
-import { Input, Row, Col } from "antd";
+import { Row, Col, Divider } from "antd";
 
 import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
 import "./index.css";
@@ -22,10 +23,10 @@ function App() {
 
   return (
     <div>
-      Hello this is react!
+      <Divider orientation="left">Search</Divider>
       <Row gutter={16} align="middle" justify="center">
         <Col span={18}>
-          <Input placeholder="Add module" />
+          <SelectModules mods={mods} />
         </Col>
         <Col span={6}>
           <UpdateModuleButton onClick={setMods}>
@@ -33,6 +34,7 @@ function App() {
           </UpdateModuleButton>
         </Col>
       </Row>
+      <Divider orientation="left">All modules</Divider>
       <ModuleTable mods={mods} />
     </div>
   );

apps/elixir_app_web/assets/js/react/components/ModuleTable.js
@@ -5,7 +5,6 @@ import ModuleBubble from "./ModuleBubble";
 function ModuleTable(props) {
   const { mods } = props;
   const len = mods.length;
-  const rows = len / 6;
 
   return (
     <Row gutter={[16, 16]}>

commit 17c6c4841ac9fce2f2b827d58ca181547ed39f7f

Fix breaking bug caused by 17bbe1c3e6515686879ad2253c806ccaa3e9e263

apps/elixir_app_web/assets/js/react/components/SelectModules.js
@@ -21,7 +21,6 @@ function SelectModules(props) {
         allowClear
         style={{ width: "100%" }}
         placeholder="Select your modules here"
-        onChange={handleChange}
       >
         {children}
       </Select>