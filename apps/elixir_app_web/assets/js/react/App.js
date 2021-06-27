yongxiangng/elixir_app

commit 17bbe1c3e6515686879ad2253c806ccaa3e9e263

Add selection bar for modules

apps/elixir_app_web/assets/js/react/App.js
@@ -1,7 +1,8 @@
 import ModuleTable from "./components/ModuleTable";
 import UpdateModuleButton from "./components/UpdateModuleButton";
+import SelectModules from "./components/SelectModules";
 import React, { useState, useEffect } from "react";
-import { Input, Row, Col } from "antd";
+import { Row, Col, Divider } from "antd";
 
 import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
 import "./index.css";
@@ -22,10 +23,10 @@ function App() {
 
   return (
     <div>
-      Hello this is react!
+      <Divider orientation="left">Search</Divider>
       <Row gutter={16} align="middle" justify="center">
         <Col span={18}>
-          <Input placeholder="Add module" />
+          <SelectModules mods={mods} />
         </Col>
         <Col span={6}>
           <UpdateModuleButton onClick={setMods}>
@@ -33,6 +34,7 @@ function App() {
           </UpdateModuleButton>
         </Col>
       </Row>
+      <Divider orientation="left">All modules</Divider>
       <ModuleTable mods={mods} />
     </div>
   );

apps/elixir_app_web/assets/js/react/components/SelectModules.js
@@ -1,0 +1,32 @@
+import React from "react";
+import { Select } from "antd";
+
+function SelectModules(props) {
+  const { Option } = Select;
+  const { mods } = props;
+
+  const children = [];
+  mods.forEach((mod) => {
+    children.push(
+      <Option key={`${mod.module_code} ${mod.module_title}`}>
+        {`${mod.module_code} ${mod.module_title}`}
+      </Option>
+    );
+  });
+
+  return (
+    <>
+      <Select
+        mode="multiple"
+        allowClear
+        style={{ width: "100%" }}
+        placeholder="Select your modules here"
+        onChange={handleChange}
+      >
+        {children}
+      </Select>
+    </>
+  );
+}
+
+export default SelectModules;

apps/elixir_app_web/assets/js/react/components/ModuleTable.js
@@ -5,7 +5,6 @@ import ModuleBubble from "./ModuleBubble";
 function ModuleTable(props) {
   const { mods } = props;
   const len = mods.length;
-  const rows = len / 6;
 
   return (
     <Row gutter={[16, 16]}>

commit a643a7eb7dadf8d8ce4730a9014c879c0ffe655a

Fix lag issue mentioned in #5 (#6)
* Add selection bar for modules

* Fix breaking bug caused by 17bbe1c3e6515686879ad2253c806ccaa3e9e263

* Add year selector query

* Add module table for search bar

* Fix lag as described in #5

apps/elixir_app_web/assets/js/react/App.js
@@ -1,19 +1,15 @@
 import ModuleTable from "./components/ModuleTable";
-import UpdateModuleButton from "./components/UpdateModuleButton";
 import SelectModules from "./components/SelectModules";
 import SelectYear from "./components/SelectYear";
 import React, { useState, useEffect } from "react";
-import { Row, Col, Divider, Space } from "antd";
+import { Divider } from "antd";
 
 import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
 import "./index.css";
 
 function App() {
   const [mods, setMods] = useState([]);
-  const [selectedMods, setSelectedMods] = useState([]);
-  const [selectedAY, setSelectedAY] = useState(undefined);
   const local = `http://${window.location.hostname}:${window.location.port}/api/v1/list_all_modules`;
-
   useEffect(() => {
     getMods(local);
   }, []);
@@ -25,31 +21,14 @@ function App() {
   };
 
   return (
-    <Space direction="vertical">
+    <>
       <Divider orientation="left">Select year</Divider>
-      <Row gutter={16} justify="center" align="middle">
-        <Col span={18}>
-          <SelectYear callback={setSelectedAY} />
-        </Col>
-        <Col
-          span={6}
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "center",
-          }}
-        >
-          <UpdateModuleButton year={selectedAY} onClick={setMods}>
-            Update Modules
-          </UpdateModuleButton>
-        </Col>
-      </Row>
+      <SelectYear onClick={setMods} />
       <Divider orientation="left">Search</Divider>
-      <SelectModules mods={mods} callback={setSelectedMods} />
-      <ModuleTable mods={selectedMods} />
+      <SelectModules mods={mods} />
       <Divider orientation="left">All modules</Divider>
       <ModuleTable mods={mods} />
-    </Space>
+    </>
   );
 }
 

apps/elixir_app_web/assets/js/react/components/ModuleBubble.js
@@ -4,13 +4,13 @@ import { Tag, Tooltip } from "antd";
 function ModuleBubble(props) {
   const colors = [
     "default",
-    "#13a000",
-    "#34dddd",
-    "#eb9486",
-    "#9370db",
-    "#1133aa",
-    "#7e7f9a",
-    "#97a7b3",
+    "green",
+    "cyan",
+    "blue",
+    "geekblue",
+    "purple",
+    "magenta",
+    "red",
   ];
   const { module_code, module_title } = props;
   const color =
@@ -19,7 +19,12 @@ function ModuleBubble(props) {
       : module_code.match(/[0-9]/g)[0];
   return (
     <Tooltip title={module_title}>
-      <Tag color={colors[color]}>{module_code}</Tag>
+      <Tag
+        color={colors[color]}
+        style={{ width: "90%", textAlign: "center", color: "black" }}
+      >
+        {module_code}
+      </Tag>
     </Tooltip>
   );
 }

apps/elixir_app_web/assets/js/react/components/SelectModules.js
@@ -1,10 +1,11 @@
-import React from "react";
-import { Select } from "antd";
+import React, { useState } from "react";
+import { Select, Space } from "antd";
+import ModuleTable from "./ModuleTable";
 
 function SelectModules(props) {
   const { Option } = Select;
-  const { mods, callback } = props;
-
+  const { mods } = props;
+  const [selectedMods, setSelectedMods] = useState([]);
   const children = [];
   mods.forEach((mod) => {
     children.push(
@@ -23,19 +24,23 @@ function SelectModules(props) {
         module_title: info[1],
       });
     });
-    callback(newArr);
+    setSelectedMods(newArr);
   };
 
   return (
-    <Select
-      mode="multiple"
-      allowClear
-      style={{ width: "100%" }}
-      placeholder="Select your modules here"
-      onChange={handleChange}
-    >
-      {children}
-    </Select>
+    <Space direction="vertical" style={{ width: "100%" }}>
+      <Select
+        mode="multiple"
+        allowClear
+        style={{ width: "100%" }}
+        placeholder="Select your modules here"
+        onChange={handleChange}
+      >
+        {children}
+      </Select>
+
+      <ModuleTable mods={selectedMods} />
+    </Space>
   );
 }
 

apps/elixir_app_web/assets/js/react/components/SelectYear.js
@@ -1,9 +1,11 @@
-import React from "react";
-import { Select } from "antd";
+import React, { useState } from "react";
+import { Select, Row, Col } from "antd";
+import UpdateModuleButton from "./UpdateModuleButton";
 
 function SelectYear(props) {
   const { Option } = Select;
-  const { callback } = props;
+  const { onClick } = props;
+  const [selectedAY, setSelectedAY] = useState(undefined);
   const today = new Date();
 
   const currAY =
@@ -19,14 +21,30 @@ function SelectYear(props) {
   );
 
   return (
-    <Select
-      allowClear
-      style={{ width: "100%" }}
-      placeholder="Select academic year"
-      onChange={callback}
-    >
-      {children}
-    </Select>
+    <Row gutter={16} justify="center" align="middle">
+      <Col span={18}>
+        <Select
+          allowClear
+          style={{ width: "100%" }}
+          placeholder="Select academic year"
+          onChange={setSelectedAY}
+        >
+          {children}
+        </Select>
+      </Col>
+      <Col
+        span={6}
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+        }}
+      >
+        <UpdateModuleButton year={selectedAY} onClick={onClick}>
+          Update Modules
+        </UpdateModuleButton>
+      </Col>
+    </Row>
   );
 }
 
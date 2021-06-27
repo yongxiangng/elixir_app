a643a7eb7dadf8d8ce4730a9014c879c0ffe655a
Ng Yong Xiang <60974969+yongxiangng@users.noreply.github.com>
2021-06-27 16:46:57 +0800

    Fix lag issue mentioned in #5 (#6)
    * Add selection bar for modules
    
    * Fix breaking bug caused by 17bbe1c3e6515686879ad2253c806ccaa3e9e263
    
    * Add year selector query
    
    * Add module table for search bar
    
    * Fix lag as described in #5

--- apps/elixir_app_web/assets/js/react/App.js
+++ apps/elixir_app_web/assets/js/react/App.js
@@ -1,19 +1,15 @@
 import ModuleTable from "./components/ModuleTable";
-import UpdateModuleButton from "./components/UpdateModuleButton";
 import SelectModules from "./components/SelectModules";
 import SelectYear from "./components/SelectYear";
 import React, { useState, useEffect } from "react";
-import { Row, Col, Divider, Space } from "antd";
+import { Divider } from "antd";
 
 import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
 import "./index.css";
 
 function App() {
   const [mods, setMods] = useState([]);
-  const [selectedMods, setSelectedMods] = useState([]);
-  const [selectedAY, setSelectedAY] = useState(undefined);
   const local = `http://${window.location.hostname}:${window.location.port}/api/v1/list_all_modules`;
-
   useEffect(() => {
     getMods(local);
   }, []);
@@ -25,31 +21,14 @@ function App() {
   };
 
   return (
-    <Space direction="vertical">
+    <>
       <Divider orientation="left">Select year</Divider>
-      <Row gutter={16} justify="center" align="middle">
-        <Col span={18}>
-          <SelectYear callback={setSelectedAY} />
-        </Col>
-        <Col
-          span={6}
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "center",
-          }}
-        >
-          <UpdateModuleButton year={selectedAY} onClick={setMods}>
-            Update Modules
-          </UpdateModuleButton>
-        </Col>
-      </Row>
+      <SelectYear onClick={setMods} />
       <Divider orientation="left">Search</Divider>
-      <SelectModules mods={mods} callback={setSelectedMods} />
-      <ModuleTable mods={selectedMods} />
+      <SelectModules mods={mods} />
       <Divider orientation="left">All modules</Divider>
       <ModuleTable mods={mods} />
-    </Space>
+    </>
   );
 }
 
